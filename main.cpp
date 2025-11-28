13e1747ff003545a33e5df8c29beb18e1eb053a9
Rich Henry <phaistos75@gmail.com>
2025-11-28 10:52:45 -0500

    Removed struct.

--- main.cpp
+++ main.cpp
@@ -4,31 +4,31 @@
 #include <iostream>
 #include <format>
 
-struct CompletionBody {
-  std::string model;
-  std::string prompt;
-  int max_tokens;
-  float temperature;
-  NLOHMANN_DEFINE_TYPE_INTRUSIVE(CompletionBody, model, prompt, max_tokens, temperature)
-};
+nlohmann::json build_request(std::string model, std::string prompt, uint32_t n_tokens, float temperature) {
+  nlohmann::json j;
+  j["model"] = model;
+  j["prompt"] = prompt;
+  j["max_tokens"] = n_tokens;
+  j["temperature"] = temperature;
+  return j;
+}
+
+std::string build_qwen_prompt(std::string instructions, std::string query) {
+  return std::format(
+    "<|im_start|>system\n{}<|im_end|>\n<|im_start|>user\n{}<|im_end|>\n<|im_start|>assistant", instructions, query
+  );
+}
 
 int main() {
   openai::start("nokey", "", true, "http://localhost:8033/v1/");
 
-  auto model = "~/.cache/llama.cpp/qwen_qwen3-vl-8b-instruct-gguf_qwen3vl-8b-instruct-q4_k_m.gguf";
+  // this doesnt matter with llama.cpp
+  auto model = "qwen_qwen3-vl-8b-instruct-gguf_qwen3vl-8b-instruct-q4_k_m.gguf";
   auto instruction = "You are a coding agent that only replies with compilable code. Do not include any markdown, formatting, descriptions or explanatory text.";
-  auto query = "Write me the sieve of eratosthenes in Odin lang.";
+  auto query = "Write me the sieve of eratosthenes in C.";
 
-  CompletionBody cb {
-    model,
-    std::format("<|im_start|>system\n{}<|im_end|>\n<|im_start|>user\n{}<|im_end|>\n<|im_start|>assistant", instruction, query),
-    1024,
-    0.1
-  };
-
-  nlohmann::json j = cb;
-
-  auto completion = openai::completion().create(cb);
+  auto req = build_request(model, build_qwen_prompt(instruction, query), 1024, 0.1);
+  auto completion = openai::completion().create(req);
   
   std::cout << completion["choices"][0]["text"].get<std::string>() << '\n'; 
 }
