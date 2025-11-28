37539a2c60a8d6048b37164dde39f03bb05881e0
Rich Henry <phaistos75@gmail.com>
2025-11-27 21:02:53 -0500

    Added better task.

--- main.cpp
+++ main.cpp
@@ -12,18 +12,17 @@ struct CompletionBody {
   NLOHMANN_DEFINE_TYPE_INTRUSIVE(CompletionBody, model, prompt, max_tokens, temperature)
 };
 
-
 int main() {
   openai::start("nokey", "", true, "http://localhost:8033/v1/");
 
   auto model = "/home/henry/.cache/llama.cpp/qwen_qwen3-vl-8b-instruct-gguf_qwen3vl-8b-instruct-q4_k_m.gguf";
   auto instruction = "You are a coding agent that only replies with compilable code. Do not include any markdown, formatting, descriptions or explanatory text.";
-  auto query = "Write me a zig program that adds two numbers.";
+  auto query = "Write me the sieve of eratosthenes in C++.";
 
   CompletionBody cb {
     model,
     std::format("<|im_start|>system\n{}\n<|im_start|>user\n{}<|im_start|>assistant", instruction, query),
-    100,
+    1024,
     0.1
   };
 
